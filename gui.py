63a075223ce5e7de9f057cf3ce3f7d3cb3e3e4a6
Alexey Yurkevich <Alexey.Yurkevich1@gmail.com>
2018-12-16 17:20:33 +0300

    reworked gui

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 import os
 
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.factory import Factory
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty, BooleanProperty
@@ -22,41 +23,14 @@ class ScreenManagement(ScreenManager):
     pass
 
 
-class LoadDialog(FloatLayout):
-    """
-    Class for loading new file to convert
-    """
-    load = ObjectProperty(None)
-    cancel = ObjectProperty(None)
-
-
-class SaveDialog(FloatLayout):
-    """
-    Class for saving signed file
-    """
-    save = ObjectProperty(None)
-    text_input = ObjectProperty(None)
-    cancel = ObjectProperty(None)
-
-
-class ErrorDialog(FloatLayout):
-    """
-    Message box for showing errors
-    """
-    message = ObjectProperty(None)
-    close = ObjectProperty(None)
-
-
-Factory.register('LoadDialog', cls=LoadDialog)
-Factory.register('SaveDialog', cls=SaveDialog)
-
-
 class Windows(GridLayout):
     """
     SuperClass for all windows.
     It includes handling fonts,
     opening and saving file, converting to rgb
     """
+    input_file_name = ''
+    are_keys_set = False
 
     def __init__(self, *args, **kwargs):
         self.dark_color = self.convert_to_rgb(55)
@@ -65,7 +39,7 @@ class Windows(GridLayout):
         self.red_color = self.convert_to_rgb(*[251, 54, 64])
         self.blue_color = self.convert_to_rgb(*[197, 235, 195])
         self.blue_color_darker = self.convert_to_rgb(*[0, 16, 99])
-        super().__init__(*args, **kwargs)
+        super().__init__(**kwargs)
 
     @staticmethod
     def regular_font(text):
@@ -149,7 +123,7 @@ class Windows(GridLayout):
 
 class MainWindow(Windows, Screen):
     """
-    Main window, that includes file signaturing functions
+    Main window, that includes file signing functions
     """
     loadfile = ObjectProperty(None)
     savefile = ObjectProperty(None)
@@ -160,6 +134,23 @@ class MainWindow(Windows, Screen):
     def __init__(self, *args, **kwargs):
         self.input_file_name = ''
         super().__init__(*args, **kwargs)
+
+    def on_enter(self):
+        Clock.schedule_once(self.update_label)
+
+    def update_label(self, value):
+        self.ids.file_status_label.text = '[font=OpenSans-Light]File status: [/font]'
+        self.ids.file_status_label.text = self.ids.file_status_label.text + '[font=OpenSans-Regular]File is not opened.[/font]' if not Windows.input_file_name \
+            else f'[font=OpenSans-Regular]Opened file: {Windows.input_file_name.split("/")[-1]}[/font]'
+
+        self.ids.opened_status_label.text = "[font=OpenSans-Light]Key status: [/font][font=OpenSans-Regular]Keys are not set! \nPlease, set up keys in [font=OpenSans-Bold]key settings[/font] menu[/font]" if not Windows.are_keys_set \
+            else "[font=OpenSans-Light]Key status: [/font][font=OpenSans-Regular]Keys are set successfully.[/font]"
+        if encrypt.keys.get('n'):
+            self.ids.n_key_input.text = str(encrypt.keys['n'])
+            self.ids.n_key_check.text = str(encrypt.keys['n'])
+
+        if encrypt.keys.get('e'):
+            self.ids.public_key_check.text = str(encrypt.keys['e'])
 
     @property
     def create_signature(self):
@@ -179,8 +170,60 @@ class MainWindow(Windows, Screen):
         self._check_signature = value
         self._create_signature = not value
 
+    def load(self, path, filename):
+        """
+        Load file handling
+        :param path: path to file
+        :param filename: full path to file
+        :return: none
+        """
+        Windows.input_file_name = filename[0]
+        self.dismiss_popup()
+        self.update_label(1)
+
+    def sign_file(self, signature_input):
+        if Windows.are_keys_set and Windows.input_file_name:
+            try:
+                signature = encrypt.get_signature_private(Windows.input_file_name)
+                signature_input.text = str(signature)
+            except KeyError:
+                content = ErrorDialog(message="Keys error", close=self.dismiss_popup)
+                self._popup = Popup(title="Key saving", content=content,
+                                    size_hint=(0.9, 0.9))
+                self._popup.open()
+        else:
+            content = ErrorDialog(message="Signing error", close=self.dismiss_popup)
+            self._popup = Popup(title="Key saving", content=content,
+                                size_hint=(0.9, 0.9))
+            self._popup.open()
+
+    def is_signature_valid(self, _signature, _public_key, _n_key):
+        try:
+            signature = int(_signature.text)
+            e_key = int(_public_key.text)
+            n_key = int(_n_key.text)
+            public_key = {'e': e_key, 'n': n_key}
+        except Exception:
+            self.ids.is_valid_signature_label.text = ''
+            content = ErrorDialog(message="Keys are not valid", close=self.dismiss_popup)
+            self._popup = Popup(title="Key checking", content=content,
+                                size_hint=(0.9, 0.9))
+            self._popup.open()
+            return
+
+        validation = encrypt.is_signature_valid(Windows.input_file_name, signature, public_key)
+        self.ids.check_file_hash.text = str(validation[1])
+        self.ids.check_file_hash_recovered.text = str(validation[2])
+        if validation[0]:
+            self.ids.is_valid_signature_label.text = '[font=OpenSans-Bold]Signature is valid[/font]'
+        else:
+            self.ids.is_valid_signature_label.text = '[font=OpenSans-Bold]Signature is invalid[/font]'
+
 
 class SettingKeys(Windows, Screen):
+    """
+    Class of handling settings windows
+    """
 
     def save_keys(self, p_value, q_value, e_value, d_value):
         """
@@ -212,6 +255,7 @@ class SettingKeys(Windows, Screen):
 
             keys = encrypt.set_keys(**encrypt.keys)
             if encrypt.check_keys(keys):
+                Windows.are_keys_set = True
                 content = ErrorDialog(message="Saved successful.", close=self.dismiss_popup)
                 self._popup = Popup(title="Key saving", content=content,
                                     size_hint=(0.9, 0.9))
@@ -219,7 +263,7 @@ class SettingKeys(Windows, Screen):
             else:
                 raise Exception
         except Exception:
-
+            Windows.are_keys_set = False
             content = ErrorDialog(message="Filling error", close=self.dismiss_popup)
             self._popup = Popup(title="Key saving", content=content,
                                 size_hint=(0.9, 0.9))
@@ -256,6 +300,31 @@ class SettingKeys(Windows, Screen):
             self._popup.open()
 
 
+class LoadDialog(FloatLayout):
+    """
+    Class for loading new file to convert
+    """
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+
+class SaveDialog(FloatLayout):
+    """
+    Class for saving signed file
+    """
+    save = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+
+class ErrorDialog(FloatLayout):
+    """
+    Message box for showing errors
+    """
+    message = ObjectProperty(None)
+    close = ObjectProperty(None)
+
+
 presentation = Builder.load_file("rsasignature.kv")
 
 
@@ -267,6 +336,13 @@ class RSASignatureApp(App):
 
     def build(self):
         return presentation
+
+
+Factory.register('MainWindow', cls=MainWindow)
+Factory.register('SettingKeys', cls=SettingKeys)
+Factory.register('ErrorDialog', cls=ErrorDialog)
+Factory.register('LoadDialog', cls=LoadDialog)
+Factory.register('SaveDialog', cls=SaveDialog)
 
 
 if __name__ == "__main__":
